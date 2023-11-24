0c9f1421c236fae2645ca5963f24969ffb85bdaf
leilaotieno2 <leila.otieno@student.moringaschool.com>
2023-11-24 03:40:36 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/components/**/*.jsx",
+    // add other file paths as needed
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
